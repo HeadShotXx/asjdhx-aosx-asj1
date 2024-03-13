22cc9fb452945af55e026cc3a3dacef6ec8b82aa
HeadShotXx <148909166+HeadShotXx@users.noreply.github.com>
2024-03-13 18:37:18 +0300

    Update gaysex.ps1

--- gaysex.ps1
+++ gaysex.ps1
@@ -1,1 +1,3 @@
 Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\fatih.bat" -ErrorAction SilentlyContinue
+Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\antimalware.exe" -ErrorAction SilentlyContinue
+
